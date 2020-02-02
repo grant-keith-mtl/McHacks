21976d93281b21a37e0b1b8a4a4b746a197cc2ca
Grant Keith <grant.keith@mail.mcgill.ca>
2020-02-02 17:06:21 -0500

    final mchacks version

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function newPlan(){
-  var icon = document.getElementById("plus_sign")
+  var icon = document.getElementById("plus_sign");
+  var newPlans = document.getElementById("newPlans");
   if(icon.className == "fas fa-plus"){
     icon.className = "fas fa-minus";
+    newPlans.style.display = "block";
   }
   else{
     icon.className = "fas fa-plus";
+    newPlans.style.display = "none";
   }
  
 }
